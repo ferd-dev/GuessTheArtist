9685ffa7d0e9488037426a27ff509cb3ceebe4b2
Fernando <vluisfernando62@gmail.com>
2024-05-16 16:03:38 -0400

    Commit dependencies.

--- obj/Debug/net8.0/GuessTheArtist.AssemblyInfo.cs
+++ obj/Debug/net8.0/GuessTheArtist.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GuessTheArtist")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94e654dfdccc21462d1cdf02ad07baba50cd6e97")]
 [assembly: System.Reflection.AssemblyProductAttribute("GuessTheArtist")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GuessTheArtist")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
